abcb2cde6704c4d9419e04ac6f80edb818694e64
kkstone <10545609+kkstone@users.noreply.github.com>
2025-08-07 10:09:06 +0800

    Add wait user script exist

--- luci-app-natmap/root/etc/natmap/notify.sh
+++ luci-app-natmap/root/etc/natmap/notify.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 SECTION_NAME="$NATMAP_SECTION_NAME"
-
 PUBLIC_IPV4="$1"
 PUBLIC_PORT="$2"
+NATMAP_ARGS="$@"
 
 if [ -z "$SECTION_NAME" ] || [ -z "$PUBLIC_IPV4" ] || [ -z "$PUBLIC_PORT" ]; then
     exit 1
@@ -14,6 +14,36 @@ echo "${PUBLIC_IPV4}:${PUBLIC_PORT}" > "${STATUS_DIR}/${SECTION_NAME}"
 
 USER_COMMAND=$(uci get natmap."$SECTION_NAME".notify_command || true)
 
-if [ -n "$USER_COMMAND" ]; then
-    eval "$USER_COMMAND" "$@"
+if [ -z "$USER_COMMAND" ]; then
+    exit 0
 fi
+
+USER_SCRIPT_PATH=$(echo "$USER_COMMAND" | awk '{print $1}')
+
+execute_user_command() {
+    eval "$USER_COMMAND" "$NATMAP_ARGS"
+}
+
+wait_and_execute_in_background() {
+    (
+        logger -t natmap-notify "Waiting for script ${USER_SCRIPT_PATH} to appear for instance ${SECTION_NAME}..."
+        while [ ! -f "$USER_SCRIPT_PATH" ]; do
+            sleep 5
+        done
+
+        logger -t natmap-notify "Script ${USER_SCRIPT_PATH} found. Executing for instance ${SECTION_NAME}."
+        execute_user_command
+    ) &
+}
+
+if echo "$USER_SCRIPT_PATH" | grep -q '/'; then
+    if [ -f "$USER_SCRIPT_PATH" ]; then
+        execute_user_command
+    else
+        wait_and_execute_in_background
+    fi
+else
+    execute_user_command
+fi
+
+exit 0
